ed7679a69e29d6a77b6602674bbb7969645e79ef
paskalo4 <paskal.trstenjak@hotmail.com>
2025-03-17 15:48:24 +0000

    Implemented CountingSort Algorithm

--- main.cpp
+++ main.cpp
@@ -25,6 +25,49 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
     for (int i = 0; i<velikost; i++)
         output << polje[i] << ' ';
 }
+void Counting_Sort(vector<int>& A) {
+    // Min, Max v A
+    int min_val = A[0];
+    int max_val = A[0];
+    for (int i = 1; i < A.size(); i++) {
+        if (A[i] < min_val) {
+            min_val = A[i];
+        }
+        if (A[i] > max_val) {
+            max_val = A[i];
+        }
+    }
+
+    int range = max_val - min_val + 1; // Razpon števil
+    vector<int> C(range, 0); // Pomožno polje C, inicializirano na 0
+
+    // Prilagodimo vrednosti v A na pozitiven interval
+    for (int i = 0; i < A.size(); i++) {
+        A[i] -= min_val;  // Pomaknemo vse vrednosti tako, da so pozitivne
+    }
+
+    for (int num : A) {
+        C[num]++;
+    }
+
+    // Seštevanje vrednosti v C za prilagoditev indeksa
+    for (int i = 1; i < range; i++) {
+        C[i] += C[i - 1];
+    }
+
+    vector<int> B(A.size());
+    // Populiramo polje B in zmanjšamo število v C
+    for (int i = A.size() - 1; i >= 0; i--) {
+        B[C[A[i]] - 1] = A[i];
+        C[A[i]]--;
+    }
+    // Kopiramo izhodno polje B nazaj v A
+    A = B;
+    // Po sortiranju vrnemo vrednosti nazaj v izvorni interval [min, max]
+    for (int i = 0; i < A.size(); i++) {
+        A[i] += min_val;  // Pomaknemo nazaj v izvorni interval
+    }
+}
 
 int main(int argc, const char* argv[]) {
     vector<int> A;
@@ -34,6 +77,7 @@ int main(int argc, const char* argv[]) {
 
     if (argv[1][0] == '0') {
         //counting sort
+        Counting_Sort(A);
     }
     else {
         //Roman sort
